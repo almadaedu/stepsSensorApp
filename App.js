ef0c604214af5e4d0c3163da4b7b6abcdd41285d
Eduardo Almada <56881337+almadaedu@users.noreply.github.com>
2023-06-08 23:43:19 -0300

    FEAT: ADDING PEDOMETER

--- App.js
+++ App.js
@@ -1,61 +1,143 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
 import { StatusBar } from "expo-status-bar";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Dimensions,
+} from "react-native";
+
 import { Pedometer } from "expo-sensors";
 
-export default function App() {
+import CircularProgress from "react-native-circular-progress-indicator";
 
-  const [PedometerAvailability, setPedometerAvailability] = useState("");
-  const [stepCount, setStepCount] = useState(0)
+export default function App() {
+  const [PedomaterAvailability, SetPedomaterAvailability] = useState("");
+
+  const [StepCount, SetStepCount] = useState(0);
+
+  var WindowHeight = Dimensions.get("window").height;
+
+  var Dist = StepCount / 1300;
+
+  var DistanceCovered = Dist.toFixed(4);
+
+  var cal = DistanceCovered * 60;
+
+  var caloriesBurnt = cal.toFixed(4);
 
   React.useEffect(() => {
-
     subscribe();
- 
   }, []);
 
-  subscribe = () => {
-
+  const subscribe = () => {
     const subscription = Pedometer.watchStepCount((result) => {
- 
-      setStepCount(result.steps);
- 
+      SetStepCount(result.steps);
     });
 
-    Pedometer.isAvailableAsync().then((result) => {
-      setPedometerAvailability(String(result))
-    
-    }, (error) =>{
-      setPedometerAvailability(error)
-    });
-  }
+    Pedometer.isAvailableAsync().then(
+      (result) => {
+        SetPedomaterAvailability(String(result));
+      },
+
+      (error) => {
+        SetPedomaterAvailability(error);
+      }
+    );
+  };
 
   return (
-    <ImageBackground
-      style={{ flex: 1 }}
-      resizeMode="cover"
-      source={require("./assets/runningBg.png")}
-    >
-      <View style={styles.container}>
-        <Text style={styles.headingDesign}>
-          Pedometer available : {PedometerAvailability}
-        </Text>
-        <Text>{stepCount}</Text>
-      </View>
-    </ImageBackground>
+    <View style={styles.container}>
+      <ImageBackground
+        style={{ flex: 1 }}
+        source={require("./assets/runningBg.png")}
+        resizeMode="cover"
+      >
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Text style={styles.headingDesign}>
+            Is Pedometer available on the device : {PedomaterAvailability}
+          </Text>
+        </View>
+
+        <View style={{ flex: 3 }}>
+          <CircularProgress
+            value={StepCount}
+            maxValue={6500}
+            radius={190}
+            textColor={"#ecf0f1"}
+            activeStrokeColor={"#f39c12"}
+            inActiveStrokeColor={"#9b59b6"}
+            inActiveStrokeOpacity={0.5}
+            inActiveStrokeWidth={30}
+            activeStrokeWidth={30}
+            title={"Step Count"}
+            titleColor={"black"}
+            titleStyle={{ fontWeight: "bold",}}
+          />
+        </View>
+
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <View style={{ flex: 1 }}>
+            <Text
+              style={[
+                styles.textDesign,
+
+                { width: "93%", paddingLeft: 20, marginLeft: "-3.5%" },
+              ]}
+            >
+              Distance Covered : {DistanceCovered} km
+            </Text>
+          </View>
+
+          <View style={{ flex: 1 }}>
+            <Text
+              style={[
+                styles.textDesign,
+
+                { paddingLeft: 10, marginLeft: "23%" },
+              ]}
+            >
+              Calories Burnt : {caloriesBurnt}
+            </Text>
+          </View>
+
+          <StatusBar style="auto" />
+        </View>
+      </ImageBackground>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#fff",
   },
+
   headingDesign: {
-    color: "white",
-    backgroundColor: "rgb(155,89,182)",
+
+    backgroundColor: "rgba(155, 89, 182,0.5)",
     alignSelf: "center",
     fontSize: 20,
+    color: "white",
+    fontWeight: "bold",
+    fontFamily: "Roboto",
+  },
+
+  textDesign: {
+
+    backgroundColor: "rgba(155, 89, 182,0.5)",
+    height: 50,
+    width: "85%",
+    borderColor: "white",
+    borderWidth: 1,
+    borderRadius: 20,
+    overflow: "hidden",
+    fontSize: 25,
+    color: "white",
     fontWeight: "bold",
     fontFamily: "Roboto",
   },
